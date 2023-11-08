64f992e5a1ed4ac8c03423584448d915278e3a37
Rupesh Raj Karn <rkarn@masdar.ac.ae>
2023-11-08 14:33:16 +0400

    code

--- Wrong-key-outcome/plot_code.py
+++ Wrong-key-outcome/plot_code.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 10})
-os.chdir('C:/Users/rrk307/Documents/GitHub/Locking-DT-RF/Brute-force-outcome')
+plt.rcParams.update({'font.size': 11})
+os.chdir('C:/Users/rrk307/Documents/GitHub/Locking-DT-RF/Wrong-key-outcome')
 Acc_dict = {}
 acc_hold_temporary = []
 correct_key_acc_Accdel=[0.6323]
@@ -25,16 +25,16 @@ for dt_name in Acc_dict.keys():
 
 c=['r','g','b','c','m']
 m=['s','o','v','^','.']
-fig, ax = plt.subplots(5,1,figsize=(4.5,5))
+fig, ax = plt.subplots(5,1,figsize=(5,5))
 for i,dt_name in enumerate(Acc_dict.keys()):
     ax[i].plot(np.arange(1,len(Acc_dict[dt_name])+1), Acc_dict[dt_name], linestyle=':', color = c[0], fillstyle='none', label='Incorrect key')
     ax[i].plot(np.arange(1,len(Acc_dict[dt_name])+1), correct_key_acc[i] * len(Acc_dict[dt_name]) , linestyle='dashed', color = c[1], fillstyle='none', label='Correct key')
-    ax[i].grid(color='k', linestyle=':', linewidth=1)
+    ax[i].grid(color='y', linestyle=':', linewidth=1)
     ax[i].set_title(f'DT accuracy for {dt_name}', fontsize=10)
     ax[i].set_yticks([0, 0.25, 0.5, 0.75, 1])
     ax[i].xaxis.set_tick_params(labelbottom=False)
     if i==3: ax[i].set_ylabel('Accuracy (* 100%)') 
-    if i==2: ax[i].legend(ncol=2, fontsize = 8)
+    if i==2: ax[i].legend(ncol=2, framealpha=0.3, fontsize = 10)
 ax[i].set_xlabel('Iterations (1 to 100)', fontsize=10)
 ax[i].xaxis.set_tick_params(labelbottom=True)
 fig.tight_layout(pad=0.2)
